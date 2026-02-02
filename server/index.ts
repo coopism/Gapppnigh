89044582d039aa8da10130db8b77e27f11ed301f
coopism <coopism@users.noreply.github.com>
2026-02-02 19:56:13 +1100

    Enable database seeding on startup

--- server/index.ts
+++ server/index.ts
@@ -65,7 +65,7 @@ app.use((req, res, next) => {
 });
 
 (async () => {
-  // await bootstrapDatabase();
+  await bootstrapDatabase();
   await registerRoutes(httpServer, app);
 
   app.use((err: any, _req: Request, res: Response, next: NextFunction) => {
